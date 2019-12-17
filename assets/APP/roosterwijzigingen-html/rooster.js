446974115032d866f7dada356a0045d7632fa4fe
LucasLeviOlling <58148335+LucasLeviOlling@users.noreply.github.com>
2019-12-17 13:45:57 +0100

    Version 1.0

---
+++ assets/APP/roosterwijzigingen-html/rooster.js
@@ -0,0 +1,3 @@
+var d = new Date();
+var n = d.getFullYear();
+document.getElementById("ditJaar").innerHTML = `© Copyright 2019-${n} Lucas Olling & Dion Frankes <br>&nbsp; &nbsp; - Alle rechten voorbehouden`;
